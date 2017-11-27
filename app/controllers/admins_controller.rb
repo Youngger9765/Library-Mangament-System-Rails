7e5e6e1404da36ae7041b6f74a8058c56add4557
Shoumma Shams <shoumma@gmail.com>
2017-11-26 18:46:48 -0800

    Readdition of assets
    Stylesheets, javascript, and images due to removal by pull request.
    Initial routes and controllers

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,18 @@
+class AdminsController < ApplicationController
+	before_action :authenticate_admin!
+
+	def login
+
+	end
+
+	def main
+		@admin = Admin.find(params[:id])
+		@books = Book.all
+		@users = User.all
+	end
+
+	def new
+
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
+  # resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  get '/admins/new', to: 'admins#new'
+  get '/admins/new', to: 'admins#login'
+  get '/users/new', to: 'user#new'
+  get '/users/login', to:'users#login'
 end
