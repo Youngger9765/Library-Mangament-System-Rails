7d94fb045a52fd6585653b7abf98d7694e8da91f
Tzu Yang <purpleice9765@msn.com>
2019-05-13 18:36:56 +0800

    debug

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Project2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(#{config.root}/lib) 
   end
 end
 
