3db6972ccf6cb30b0f5cdf102680d045d1b6ccc8
Tzu Yang <purpleice9765@msn.com>
2019-05-13 14:37:38 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # devise_for :admins
-  devise_for :users
-  # :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  get '/users/auth/google_oauth2/callback', to: 'user/omniauth_callbacks#google_oauth2'
+  # get '/users/auth/google_oauth2/callback', to: 'users/omniauth_callbacks#google_oauth2'
 
   # resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
